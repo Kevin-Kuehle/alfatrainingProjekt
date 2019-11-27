78ef31c936c886c873a8d04b6d8cab1b15a0dde2
K.K <kevin.kuehle@googlemail.com>
2019-11-27 21:06:43 +0100

    do something ux

--- src/app/bilder/bilder.component.ts
+++ src/app/bilder/bilder.component.ts
@@ -1,49 +1,49 @@
-import {Component, OnInit} from '@angular/core';
-import {ImagesService} from "../images.service";
-import {Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { ImagesService } from '../images.service';
+import { Router } from '@angular/router';
 
-@Component( {
+@Component({
   selector: 'app-bilder',
   templateUrl: './bilder.component.html',
   styleUrls: ['./bilder.component.scss']
-} )
+})
 
 export class BilderComponent implements OnInit {
   public title = 'Schöne Bilder von Pixabay ♥';
-  private data;
+  public data;
   loggedIn = false;
 
-  constructor( private imageService: ImagesService, private router: Router ) { }
+  constructor(private imageService: ImagesService, private router: Router) { }
 
   ngOnInit() {
     // @ts-ignore
-    this.imageService.getData().then( ( resolve, err ) => {
-      if ( err ) console.log( err );
+    this.imageService.getData().then((resolve, err) => {
+      if (err) { console.log(err); }
       this.data = resolve;
-    } );
+    });
 
-    if ( sessionStorage.getItem( 'loggedIn' ) != null && sessionStorage.getItem( 'loggedIn' ) == 'true' ) {
+    if (sessionStorage.getItem('loggedIn') != null && sessionStorage.getItem('loggedIn') === 'true') {
       this.loggedIn = true;
     }
   }
 
-  showDetail( index ) {
-    this.router.navigate( ['Bild', index] );
+  showDetail(index) {
+    this.router.navigate(['Bild', index]);
   }
 
-  newSearch( toSearch ) {
+  newSearch(toSearch) {
     // @ts-ignore
-    this.imageService.getNewData( toSearch ).then( ( res, err ) => {
-      if ( err ) console.log( err );
+    this.imageService.getNewData(toSearch).then((res, err) => {
+      if (err) { console.log(err); }
       this.data = [...res[0]];
-    } )
+    });
   }
 
-  changeImageLimit( limit ) {
+  changeImageLimit(limit) {
     // @ts-ignore
-    this.imageService.changeResultLimit( limit ).then( ( res, err ) => {
-      if ( err ) console.log( err );
+    this.imageService.changeResultLimit(limit).then((res, err) => {
+      if (err) { console.log(err); }
       this.data = [...res];
-    } );
+    });
   }
 }
